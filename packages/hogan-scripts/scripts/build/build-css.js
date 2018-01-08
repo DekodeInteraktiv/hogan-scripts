01e8f0ce1d13148fe148d4d31d1dc7d10ce1b1eb
Petter Walbø Johnsgård <petter@dekode.no>
2018-01-08 13:47:46 +0100

    Make it posible to set object in hogan css config

--- packages/hogan-scripts/scripts/build/build-css.js
+++ packages/hogan-scripts/scripts/build/build-css.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+const _ = require( 'lodash' );
 const autoprefixer = require( 'autoprefixer' );
 const chalk = require( 'chalk' );
 const cssnano = require( 'cssnano' );
@@ -20,47 +21,58 @@ const browsers = require( '../../config/browsers' );
  * Variables
  */
 const shouldUseSourceMap = process.env.NODE_ENV !== 'production';
-const name = pluginPackage.hogan.name || 'hogan';
-const from = pluginPackage.hogan.css;
-const to = path.join( paths.buildFolder, `${ name }.css` );
 
-/**
- * Config
- */
-const config = {
-	from,
-	to,
-};
-
-if ( shouldUseSourceMap ) {
-	config.map = {
-		inline: false,
-	};
-}
-
-/**
+/*
  * Build
  */
+function build( from, to ) {
+	/**
+	 * Config
+	 */
+	const config = {
+		from,
+		to,
+	};
+
+	if ( shouldUseSourceMap ) {
+		config.map = {
+			inline: false,
+		};
+	}
+
+	fs.readFile( from, ( err, css ) => {
+		postcss( [
+			postcssFlexbugsFixes,
+			autoprefixer( { browsers, flexbox: 'no-2009' } ),
+			cssnano,
+		] )
+			.process( css, config )
+			.then( result => {
+				fs.writeFile( to, result.css, () => {
+					console.log( chalk.green( 'Compiled CSS successfully.' ) );
+				} );
+
+				if ( shouldUseSourceMap ) {
+					fs.writeFile( `${ to }.map`, result.map, () => {
+						console.log( chalk.green( 'Compiled CSS map successfully.' ) );
+					} );
+				}
+			} );
+	} );
+}
+
 module.exports = () => {
 	if ( pluginPackage.hogan.css ) {
-		fs.readFile( from, ( err, css ) => {
-			postcss( [
-				postcssFlexbugsFixes,
-				autoprefixer( { browsers, flexbox: 'no-2009' } ),
-				cssnano,
-			] )
-				.process( css, config )
-				.then( result => {
-					fs.writeFile( to, result.css, () => {
-						console.log( chalk.green( 'Compiled CSS successfully.' ) );
-					} );
+		if ( _.isObjectLike( pluginPackage.hogan.css ) ) {
+			_.forEach( pluginPackage.hogan.css, ( value, key ) => {
+				build( value, path.join( paths.buildFolder, `${ key }.css` ) );
+			} );
+		} else {
+			const name = pluginPackage.hogan.name || 'hogan';
+			const from = pluginPackage.hogan.css;
+			const to = path.join( paths.buildFolder, `${ name }.css` );
 
-					if ( shouldUseSourceMap ) {
-						fs.writeFile( `${ to }.map`, result.map, () => {
-							console.log( chalk.green( 'Compiled CSS map successfully.' ) );
-						} );
-					}
-				} );
-		} );
+			build( from, to );
+		}
 	}
 };

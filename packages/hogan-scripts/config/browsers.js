bbaff94fd2a5013e6e16158495870384f7e4c139
Petter Walbø Johnsgård <petter@dekode.no>
2018-04-25 11:25:29 +0200

    Use single quotes and add missing comma dangle

--- packages/hogan-scripts/config/browsers.js
+++ packages/hogan-scripts/config/browsers.js
@@ -1,8 +1,8 @@
 module.exports = [
-	"> 1%",
-	"Edge >= 12",
-	"Explorer >= 10",
-	"iOS >= 8",
-	"Android >= 4.4",
-	"last 2 versions"
+	'> 1%',
+	'Edge >= 12',
+	'Explorer >= 10',
+	'iOS >= 8',
+	'Android >= 4.4',
+	'last 2 versions',
 ];
